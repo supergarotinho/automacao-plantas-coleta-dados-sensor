fea3ef63e6003d4c1f5699e58005e4005b13f311
Anderson Santos <anderson@gruponeuro.com.br>
2018-08-26 15:12:17 -0300

    primeira versao

---
+++ ler_sensor.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+# Start by importing the libraries we want to use
+
+import RPi.GPIO as GPIO
+import time
+
+def callback(channel):
+    if GPIO.input(channel):
+        print("No")
+    else:
+        print("Yes")
+
+# Set our GPIO numbering to BCM
+GPIO.setmode(GPIO.BCM)
+
+# Define the GPIO pin that we have our digital output from our sensor connected to
+channel = 17
+# Set the GPIO pin to an input
+GPIO.setup(channel, GPIO.IN)
+
+# This line tells our script to keep an eye on our gpio pin and let us know when the pin goes HIGH or LOW
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
+# This line asigns a function to the GPIO pin so that when the above line tells us there is a change on the pin, run this function
+GPIO.add_event_callback(channel, callback)
+
+# This is an infinte loop to keep our script running
+while True:
+	# This line simply tells our script to wait 0.1 of a second, this is so the script doesnt hog all of the CPU
+	time.sleep(0.1)
